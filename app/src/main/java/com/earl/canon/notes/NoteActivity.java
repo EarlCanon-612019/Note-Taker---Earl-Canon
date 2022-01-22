6f50ad9cbef5f46b2bdca5165a2d1db21d3db229
EarlCanon-612019 <71628798+EarlCanon-612019@users.noreply.github.com>
2022-01-22 16:43:30 +0400

    FINAL OUTPUT UTILITY APP
    A simple yet convenient note-taking app

---
+++ app/src/main/java/com/earl/canon/notes/NoteActivity.java
@@ -0,0 +1,48 @@
+package com.earl.canon.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class NoteActivity extends AppCompatActivity {
+    private EditText editText;
+    private Button deleteButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+
+        Intent intent = getIntent();
+        editText = findViewById(R.id.note_edit_text);
+        editText.setText(intent.getStringExtra("content"));
+
+        deleteButton = findViewById(R.id.button);
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = getIntent();
+                int id = intent.getIntExtra("id", 0);
+                MainActivity.database.noteDao().delete(id);
+                finish();
+                Toast.makeText(getApplicationContext(),"Deleted!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Intent intent = getIntent();
+        int id = intent.getIntExtra("id", 0);
+        MainActivity.database.noteDao().save(editText.getText().toString(), id);
+    }
+}
